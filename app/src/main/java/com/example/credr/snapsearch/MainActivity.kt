86d5bbcec8c3d6675c92f26d6df78f96e47c1c6f
droidwala <punitdama@gmail.com>
2017-12-31 11:20:40 +0530

    MainActivity + SearchActivtiy UI

--- app/src/main/java/com/example/credr/snapsearch/MainActivity.kt
+++ app/src/main/java/com/example/credr/snapsearch/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.example.credr.snapsearch
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import com.example.credr.snapsearch.search.SearchActivity
+import kotlinx.android.synthetic.main.toolbar_with_search.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -9,5 +12,9 @@ class MainActivity : AppCompatActivity() {
         setTheme(R.style.AppTheme)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        search_box.setOnClickListener {
+            startActivity(Intent(this,SearchActivity::class.java))
+        }
     }
 }
--- app/src/main/java/com/example/credr/snapsearch/search/SearchActivity.kt
+++ app/src/main/java/com/example/credr/snapsearch/search/SearchActivity.kt
@@ -2,6 +2,8 @@ package com.example.credr.snapsearch.search
 
 import android.app.Activity
 import android.os.Bundle
+import com.example.credr.snapsearch.R
+import kotlinx.android.synthetic.main.activity_search.*
 
 /**
  * Created by punitdama on 30/12/17.
@@ -10,5 +12,8 @@ class SearchActivity : Activity(){
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_search)
+
+        back_button.setOnClickListener { finish() }
     }
 }
